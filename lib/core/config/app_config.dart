23a38922f567dd997071191b0cf54e68d60f993c
MartinDiCe <jamartindice@gmail.com>
2026-06-22 21:13:33 -0300

    Bump mobile build to 1.0.6+55

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -26,7 +26,7 @@ class AppConfig {
   );
   static const String appBuildNumber = String.fromEnvironment(
     'APP_BUILD_NUMBER',
-    defaultValue: '54',
+    defaultValue: '55',
   );
   static const String reviewerUsername = String.fromEnvironment(
     'REVIEWER_USERNAME',
